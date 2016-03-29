f577cc4f3d27d05b70ee076a7ebe2552546e6875
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-03-29 12:47:55 -0700

    Footer & Configuration
    Started work on the template's footer (styles, HTML, et cetera).  Currently lacking a contact form which will be added later.
    
    Added a YAML-based (using Symfony's YAML component) configuration system to populate details in the footer (phone numbers, addresses, et cetera).  This configuration file can also be used to select the theme.  The configuration file included in this commit lacks any Mission Inn-specific information and is named "sample-settings.yml" (rather than "settings.yml" which is what the code actually looks for) as per Shawn's request.

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
 
 	// set up page data 
 	
-	$theme='missioninn';
+	$autoloader=require_once('vendor/autoload.php');
+	$config_file=dirname(__FILE__).'/config/settings.yml';
+	$config=@file_get_contents($config_file);
+	if ($config===false) die('Could not read '.$config_file);
+	$config=\Symfony\Component\Yaml\Yaml::parse($config);
+	//	TODO: Check schema?
+	if (!isset($config['theme']) && is_string($config['theme'])) die('No theme');
+	$theme=$config['theme'];
 	
 	$get_slideshow=function () {
 		
@@ -37,6 +44,7 @@
 		$data['posts'] = Timber::get_posts();
 		
 	endif;
+	$data['config']=$config;
     $data['menu'] = new TimberMenu();
 	if ( is_single() ) :
 		$data['page'] = 'single';	
@@ -73,6 +81,6 @@
     $data['base_template'] = 'base.twig';
 	
 	// render using Twig template index.twig
-	Timber::$dirname='views/'.$theme;
+	Timber::$locations=dirname(__FILE__).'/views/'.$theme;
 	Timber::render('wp/' . $template . '.twig', $data );
 ?>
