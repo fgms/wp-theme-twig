d11e5ee748ca12268ed9076f566403506cab7516
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-03-30 16:18:37 -0700

    [tabs] & [tab] Shortcodes
    Example:
    
    [tabs]
    	[tab name="First"]Hello world![/tab]
    	[tab name="Second" active="true"]The second tab which is active by default[/tab]
    	[tab name="Third" id="special_id"]The third tab which has a special ID instead of an automatically generated one[/tab]
    [/tabs]

--- functions.php
+++ functions.php
@@ -100,3 +100,94 @@ function fgms_meta_boxes ($bs) {
 	return $bs;
 	
 }
+
+
+call_user_func(function () {
+	
+	$num=0;
+	$tabs=null;
+	//	Tabs are going to be an object:
+	//
+	//	{
+	//		title:
+	//		id:
+	//		active:
+	//		content:
+	//	}
+	
+	add_shortcode('tabs',function ($atts, $content) use (&$tabs, &$num) {
+		
+		$tabs=array();
+		
+		//	Ignore the content: Any content that isn't
+		//	delivered through a [tab] shortcode is ignored
+		do_shortcode($content);
+		
+		$c=count($tabs);
+		
+		//	Preprocess tabs:
+		//
+		//	-	Determine which tab starts active
+		//	-	Set IDs for all tabs (auto generate if
+		//		not set explicitly)
+		$active=0;
+		array_walk($tabs,function ($v, $k) use (&$active, &$num) {
+			
+			if (!isset($v->id)) $v->id='t'.++$num;
+			
+			if ($v->active) $active=$k;
+			
+		});
+		
+		//	Generate tabs
+		$html='<ul class="nav nav-tabs" role="tablist">';
+		array_walk($tabs,function ($v, $k) use ($active, &$html) {
+			
+			$html.=sprintf(
+				'<li role="presentation" class="%1$s"><a href="#%2$s" role="tab" data-toggle="tab" aria-controls="%2$s">%3$s</a></li>',
+				$active===$k ? 'active' : '',
+				htmlspecialchars($v->id),
+				htmlspecialchars($v->title)
+			);
+			
+		});
+		$html.='</ul>';
+		
+		//	Generate panes
+		$html.='<div class="tab-content">';
+		array_walk($tabs,function ($v, $k) use ($active, &$html) {
+			
+			$html.=sprintf(
+				'<div role="tabpanel" class="tab-pane %1$s" id="%2$s">%3$s</div>',
+				$active===$k ? 'active' : '',
+				htmlspecialchars($v->id),
+				$v->content
+			);
+			
+		});
+		$html.='</div>';
+		
+		//	Clean state for next [tabs]
+		$tabs=null;
+		
+		return $html;
+		
+	});
+	
+	add_shortcode('tab',function ($atts, $content) use (&$tabs) {
+		
+		$atts=shortcode_atts(array('title' => '', 'id' => null, 'active' => null),$atts);
+		
+		$tabs[]=(object)array(
+			'title' => $atts['title'],
+			'content' => do_shortcode($content),
+			'id' => $atts['id'],
+			'active' => isset($atts['active']) && ($atts['active']==='true')
+		);
+		
+		//	Content is handled through other mechanisms
+		return '';
+		
+	});
+	
+});
