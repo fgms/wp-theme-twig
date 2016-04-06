ac208a9e2fa6a026384df9b91d60a31622a2879a
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-04-06 00:14:17 -0700

    [carousel] & [slide]
    Bootstrap carousels may now be added directly to posts and pages using the [carousel] and [slide] shortcodes.
    
    The [carousel] shortcode accepts several attributes:
    
    -	id: The HTML ID which should be used for the carousel, defaults to "carousel" followed by a unique integer
    -	outerclass: The HTML class which should be used for the outer part of the carousel, defaults to null
    -	innerclass: The HTML class which should be used for the inner part of the carousel, defaults to null
    -	controls: "true" or "false", determines whether there will be controls to change slides
    -	indicators: "true" or "false", determines whether the dot-indicators (indicates which slide is currently showing) will be present
    
    The [slide] shortcode may only occur within a [carousel] shortcode and accepts several attributes:
    
    -	title: The title of the slide
    -	caption: The caption which should appear under the title on the slide
    -	url: The URL of the image to use as the background for the slide
    -	alt: The alternate text for the image
    
    Any content of a [slide] shortcode is discarded.  Any content of a [carousel] shortcode beyond [slide] shortcodes is discarded.

--- functions.php
+++ functions.php
@@ -210,3 +210,69 @@ call_user_func(function () {
 	});
 	
 });
+
+
+call_user_func(function () {
+	
+	$imgs=null;
+	$id=0;
+	//	Each image/slide will be an object:
+	//
+	//	{
+	//		caption:
+	//		url:
+	//		alt:
+	//		title:
+	//	}
+	
+	add_shortcode('carousel',function ($atts, $content) use (&$imgs) {
+		
+		$imgs=array();
+		
+		//	Ignore content
+		do_shortcode($content);
+		
+		$atts=shortcode_atts(array('id' => null, 'outerclass' => null, 'innerclass' => null, 'controls' => true, 'indicators' => true),$atts);
+		if (!isset($atts['id'])) $atts['id']='carousel'.(++$id);
+		if ($atts['controls']==='false') $atts['controls']=false;
+		if ($atts['indicators']==='false') $atts['indicators']=false;
+		
+		$data=array(
+			'items' => array_map(function ($obj) {	return (array)$obj;	},$imgs),
+			'id' => $atts['id'],
+			'indicators' => $atts['indicators'],
+			'controls' => $atts['controls'],
+			'innerclass' => $atts['innerclass'],
+			'outerclass' => $atts['outerclass']
+		);
+		
+		ob_start();
+		Timber::render('bootstrap-carousel.twig',$data);
+		$retr=ob_get_contents();
+		ob_end_clean();
+		
+		$imgs=null;
+		
+		return $retr;
+		
+	});
+	
+	add_shortcode('slide',function ($atts) use (&$imgs) {
+		
+		//	If we're not in a carousel just bail out
+		if (is_null($imgs)) return '';
+		
+		$atts=shortcode_atts(array('caption' => null, 'url' => null, 'alt' => null, 'title' => null),$atts);
+		
+		$imgs[]=(object)array(
+			'title' => $atts['title'],
+			'url' => $atts['url'],
+			'alt' => $atts['alt'],
+			'caption' => $atts['caption']
+		);
+		
+		return '';
+		
+	});
+	
+});
