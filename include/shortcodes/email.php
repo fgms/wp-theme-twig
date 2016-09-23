06070f456711a0dc5a8e98c033f87d8db7a8c28a
sturple <shawn.turple@shaw.ca>
2016-09-23 11:11:08 -0700

    Removed new line character at end of file wich causes yoast sitemap not to work.

--- include/shortcodes/email.php
+++ include/shortcodes/email.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	call_user_func(function () {
 		add_shortcode('email',function ($atts, $content)  {
 			// 0 is string, 1 is prefix, 2 is domain, and 3 is the .{com | ca | info ...}
@@ -9,6 +9,6 @@
 				return '<a href="" data-prefix="' . $match[1] .'" data-domain="'. $match[2] .'.'.$match[3] .'">' . $label .'</a>';
 			}
             return 'Email was invalid -- '. $content;
-		});		
+		});
 	});
-?> 
+?>
