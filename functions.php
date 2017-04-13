ebd5dfa737922f1b2f6fad53bece6e787b24466f
sturple <shawn.turple@shaw.ca>
2017-04-13 14:43:58 -0700

    removed piklist admin options from functions.php and put into cpt plugin.

--- functions.php
+++ functions.php
@@ -329,35 +329,7 @@ function get_fg_post_type($post_type='post', $limit=3, $orderby='date') {
 	$posts =  Timber::get_posts($args);
 	return $posts;
 }
-add_filter('piklist_admin_pages', 'piklist_theme_setting_pages');
-function piklist_theme_setting_pages($pages)
-{
-	$pages[] = [
-	 'page_title' => __('Settings')
-	 ,'menu_title' => __('Settings', 'piklist')
-	 ,'sub_menu' => 'edit.php?post_type=slideshow'
-	 ,'capability' => 'manage_options'
-	 ,'menu_slug' => 'slideshow_settings'
-	 ,'setting' => 'slideshow_settings'
-	 ,'menu_icon' => plugins_url('piklist/parts/img/piklist-icon.png')
-	 ,'page_icon' => plugins_url('piklist/parts/img/piklist-page-icon-32.png')
-	 ,'single_line' => true
-	 ,'save_text' => 'Save Settings'
-	];
-	$pages[] = [
-	 'page_title' => __('Settings')
-	 ,'menu_title' => __('Settings', 'piklist')
-	 ,'sub_menu' => 'edit.php?post_type=gallery'
-	 ,'capability' => 'manage_options'
-	 ,'menu_slug' => 'gallery_settings'
-	 ,'setting' => 'gallery_settings'
-	 ,'menu_icon' => plugins_url('piklist/parts/img/piklist-icon.png')
-	 ,'page_icon' => plugins_url('piklist/parts/img/piklist-page-icon-32.png')
-	 ,'single_line' => true
-	 ,'save_text' => 'Save Settings'
-	];
-  return $pages;
-}
+
 
 add_image_size( 'piklist', 50, 50, false );
 
