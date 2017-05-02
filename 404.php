6f7839d35091eb2b002a4b88ffa52f1ba178e27d
sturple <shawn.turple@shaw.ca>
2017-05-02 10:05:56 -0700

    update 404.php to add footer widgets and sidebar widget and remove slideshow ref

--- 404.php
+++ 404.php
@@ -2,10 +2,12 @@
   $data = Timber::get_context();
   $data['posts'] = Timber::get_posts();
   $data['page'] = 'page';
-  $data['slideshow']=$get_slideshow();
   $data['config']=$get_config();
   $data['menu'] = new TimberMenu();
   require_once(__DIR__.'/include/twig-filter-functions.php');
+  $data['widget_footer_sidebar_left'] = Timber::get_widgets('widget_footer_sidebar_left');
+  $data['widget_footer_sidebar_right'] = Timber::get_widgets('widget_footer_sidebar_right');
+  $data['widget_page_sidebar'] = Timber::get_widgets('widget_page_sidebar');
   // render using Twig template index.twig
   Timber::render('page-404.twig', $data );
 ?>
